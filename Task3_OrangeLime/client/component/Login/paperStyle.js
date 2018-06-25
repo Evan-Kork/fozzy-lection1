e6d181124a4eabc6fa860c89f6a3e9805cfbc4fa
Vladislav <32749288+fon-Luchs@users.noreply.github.com>
2018-06-25 18:30:49 +0300

    Add files via upload
    Add task 3

---
+++ Task3_OrangeLime/client/component/Login/paperStyle.js
@@ -0,0 +1,13 @@
+const paperStyle = {
+    position: 'relative',
+    height: 400,
+    width: 400,
+    margin: '0 auto',
+    textAlign: 'center',
+    display: 'inline-block',
+    border: '20px solid #FF9800',
+    backgroundColor: '#76FF03',
+    zIndex: 2
+};
+
+export default paperStyle;
