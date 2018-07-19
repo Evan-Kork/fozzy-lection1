f95e371a081fcd5407c8979fd645a3669f483e89
Михайло Бачу <bachu.m@donnu.edu.ua>
2018-07-19 18:55:40 +0300

    dz string job

---
+++ task-01/dz6-bootstrap/public/js/vendors/array-js.js
@@ -0,0 +1,26 @@
+function ok() {
+	let arr = document.getElementById('array');
+	var buf =[];
+	var str ='';
+	for (var i = 0; i < arr.value.length; i++) {
+		if(arr.value[i]!=','){
+			str+=arr.value[i];
+		}
+		else{
+			buf.push(str);
+			str = '';
+		}
+		if(i ==arr.value.length-1){
+			buf.push(str);
+		}
+	}
+	console.log(buf);
+	let show = document.getElementById('show');
+	show.innerText = buf.length;
+};
+function clean(){
+	let arr = document.getElementById('array');
+	arr.value = null;
+	ok();
+};
+ok();
