8e588df61a7443af6d4ad85885f76df60390f7b7
Evan-Kork <dolg.evgen.007@gmail.com>
2018-07-21 16:54:56 +0300

    Add createCard

--- task-0/lection-09/public/js/dom-array.js
+++ task-0/lection-09/public/js/dom-array.js
@@ -12,36 +12,102 @@ console.log('hasAccordion', hasAccordionClass, hasAccordionOnclick);
 // Find by class
 let accordionCardsElement = accordionElement.getElementsByClassName('card');
 console.log('accordionCardsElement', accordionCardsElement);
+let checkLengthArray = () => {
+	let value = document.getElementById('functionLengthInput').value;
+	let valueArray = value.split(',');
+	console.log('checkLengthArray', valueArray);
+	let functionLengthResult = document.getElementsByClassName('function-length-result');
+	console.log('functionLengthResult', functionLengthResult);
+	functionLengthResult[0].innerText = 'Result => ' + valueArray.length;
+};
+let createLengthFunction = () => {
+	return `<div>
+				<input id="functionLengthInput" type="text" value="1,2,3,4,5">
+				<button id="buttonLengthArray">Chekc length</button>
+				<div class="function-length-result"></div>
+			</div>`;
+}
 let documentationArray = [
 	{
 		name: 'Lenght',
-		text: 'Length documentation'
+		text: 'Length documentation',
+		init: createLengthFunction
 	},
 	{
 		name: 'Slice',
 		text: 'Slice documentation'
+	},
+	{
+		name: 'Concat',
+		text: 'Concat documentation'
 	}
 ];
-for (let i = accordionCardsElement.length - 1; i >= 0; i--) {
-	let LinksAccordionCardElement = accordionCardsElement[i].getElementsByClassName('btn-link');
-	if (LinksAccordionCardElement.length > 0) {
-		let link = LinksAccordionCardElement[0];
-		link.innerHTML = documentationArray[i].name;
-	}
-	let lastElementChild = accordionCardsElement[i].lastElementChild;
-	let bodyAccordionCardElement = lastElementChild.getElementsByClassName('card-body');
-	if (bodyAccordionCardElement.length > 0) {
-		let body = bodyAccordionCardElement[0];
-		body.innerText = documentationArray[i].text;
-		let newElement = document.createElement('div');
-		newElement.className = 'new-element';
-		let paramElement = document.createElement('div');
-		paramElement.innerText = documentationArray[i].text;
-		newElement.appendChild(paramElement);
-		console.log('newElement', newElement);
-		body.innerHTML = '';
-		body.appendChild(newElement);
-	}
+// for (let i = accordionCardsElement.length - 1; i >= 0; i--) {
+// 	let LinksAccordionCardElement = accordionCardsElement[i].getElementsByClassName('btn-link');
+// 	if (LinksAccordionCardElement.length > 0) {
+// 		let link = LinksAccordionCardElement[0];
+// 		link.innerHTML = documentationArray[i].name;
+// 	}
+// 	let lastElementChild = accordionCardsElement[i].lastElementChild;
+// 	let bodyAccordionCardElement = lastElementChild.getElementsByClassName('card-body');
+// 	if (bodyAccordionCardElement.length > 0) {
+// 		let body = bodyAccordionCardElement[0];
+// 		body.innerText = documentationArray[i].text;
+// 		let newElement = document.createElement('div');
+// 		newElement.className = 'new-element';
+// 		let paramElement = document.createElement('div');
+// 		paramElement.innerText = documentationArray[i].text;
+// 		newElement.appendChild(paramElement);
+// 		console.log('newElement', newElement);
+// 		body.innerHTML = '';
+// 		body.appendChild(newElement);
+// 	}
+// }
+
+let documentationBlock = document.getElementById('documentation');
+let createCardHeader = (i) => {
+	let cardHeaderBlock = document.createElement('div');
+	cardHeaderBlock.className = 'card-header';
+	cardHeaderBlock.setAttribute('id', 'heading' + i);
+	cardHeaderBlock.innerHTML = `<button
+								
+									class="btn btn-link"
+									type="button"
+									data-toggle="collapse"
+									data-target="#collapse${i}"
+									aria-expanded="${i === 0 ? 'true' : 'false'}"
+									aria-controls="collapse${i}"
+								>
+									${documentationArray[i].name}
+								</button>`;
+	console.log('cardHeaderBlock', cardHeaderBlock);
+	return cardHeaderBlock;
+}
+let createBodyCard = (i) => {
+	let cardBodyBlock = document.createElement('div');
+	cardBodyBlock.setAttribute('id', 'collapse' + i);
+	cardBodyBlock.setAttribute('class', `collapse ${i === 0 ? 'show' : ''}`);
+	cardBodyBlock.setAttribute('aria-labelledby', `heading${i}`);
+	cardBodyBlock.setAttribute('data-parent', '#documentation');
+	cardBodyBlock.innerHTML = `<div class="card-body">
+									${documentationArray[i].init !== undefined ? documentationArray[i].init() : documentationArray[i].text}
+								</div>`;
+	return cardBodyBlock;
+}
+let clickButtonFunction = () => {
+	document.getElementById('buttonLengthArray').addEventListener('click', checkLengthArray);
+	
+}
+let createCard = (i) => {
+	let cardBlock = document.createElement('div');
+	cardBlock.className = 'card';
+	cardBlock.append(createCardHeader(i));
+	cardBlock.append(createBodyCard(i));
+	documentationBlock.append(cardBlock);
+	clickButtonFunction();
+}
+for (let i = 0; i < documentationArray.length; i++) {
+	createCard(i);
 }
 
 
