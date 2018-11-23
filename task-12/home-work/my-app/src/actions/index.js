ba9690ef84b84fef2a38aea5a760251ff44fb742
nesmeyanov <nesmeyanov1990@gmail.com>
2018-11-23 13:21:03 +0200

    delete all

--- task-12/home-work/my-app/src/actions/index.js
+++
@@ -1,7 +0,0 @@
-export const select = (id) => {
-	console.log('action', id);
-	return{
-		type: "CHANGE_GOODS",
-		id: id
-	}
-};
--- task-12/home-work/my-app/src/script/reducers/goods-active.js
+++
@@ -1,9 +0,0 @@
-export default function (state=null, action){
-	switch(action.type){
-		case "GOOD_SELECTED":
-			return action.payload;
-		// break;
-		default: 
-		return state;
-	}
-}
